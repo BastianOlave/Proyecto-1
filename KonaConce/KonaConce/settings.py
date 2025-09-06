dce580e9b86f7de6bb6de9413d60862f0e5d6148
Bastian <bastianolavelagos@gmail.com>
2025-09-06 14:53:36 -0400

    wa

--- KonaConce/KonaConce/settings.py
+++ KonaConce/KonaConce/settings.py
@@ -1,5 +1,5 @@
 
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -111,3 +111,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
